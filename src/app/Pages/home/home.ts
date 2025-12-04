eee87ed7be8ba13ea43e7bb5e803230c9c397c3d
Nandaku45 <nandars9052601019@gmail.com>
2025-12-04 21:17:50 +0530

    Footer is added

---
+++ src/app/Pages/home/home.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: false,
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+  image="Images/Green_basket_products.png"
+  apple="Images/Apples.webp";
+  banana="Images/Banana.jpeg";
+  Butter="Images/Butter.jpeg";
+}
--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Header } from './Components/header/header';
+import { Home } from './Pages/home/home';
+import { Footer } from './Pages/footer/footer';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Header,
+    Home,
+    Footer
   ],
   imports: [
     BrowserModule,
