036cdb249875be49e2bd9a72ae94c57078772236
Nandaku45 <nandars9052601019@gmail.com>
2025-12-04 21:43:26 +0530

    Images added to application

--- src/app/Pages/home/home.ts
+++ src/app/Pages/home/home.ts
@@ -8,7 +8,20 @@ import { Component } from '@angular/core';
 })
 export class Home {
   image="Images/Green_basket_products.png"
-  apple="Images/Apples.webp";
+  apples="Images/Apples.webp";
   banana="Images/Banana.jpeg";
-  Butter="Images/Butter.jpeg";
+  butter="Images/Butter.jpeg";
+  capsicum="Images/coloured-capsicum.webp";
+  curd="Images/Curd.jpeg";
+  eggs="Images/Eggs.jpeg";
+  onions="Images/fresh-onions.webp";
+  ghee="Images/Ghee.jpeg";
+  guava="Images/guava.webp"
+  jackfruit="Images/Jackfruit.jpeg";
+  mangoes="Images/Mangoes.webp";
+  milk="Images/Milk1.jpeg";
+  mirchi="Images/Mirchi.avif";
+  potato="Images/Potato.jpg"
+  tomato="Images/Tomato.webp";
+
 }
